21de76372be5e0f008aac132878753c33a8de057
Jeremy Cope <j.cope@me.com>
2014-05-14 18:08:27 -0400

    Initial integration of DMWPhotoBrowser.. works but view transitions not consistent. Looking into implementing a navigation view controller for the home.

--- DemoRESideMenu.h
+++ DemoRESideMenu.h
@@ -9,6 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "RESideMenu.h"
 
-@interface DemoRESideMenu : UIViewController <DemoApp>
+@interface DemoRESideMenu : UIViewController <DemoApp, RESideMenuDelegate>
 
 @end
